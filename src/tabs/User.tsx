a85a11107e6b950ba3358bafbdd106c7179ec38c
Bhupendra.Choudhary <bhupendra.choudhary@MIndrubys-Mac-mini-2.local>
2023-11-03 11:53:15 +0530

    third commit

--- src/tabs/User.tsx
+++ src/tabs/User.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
 
 const User = () => {
   return (
-    <View>
-      <Text>User</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={styles.userInfo}>This is user's page</Text>
+    </SafeAreaView>
   )
 }
 
 export default User
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1
+  },
+  userInfo:{
+    fontSize: 30,
+    fontWeight: '800',
+    alignSelf: 'center',
+    marginTop: 200,
+    color: 'black'
+
+  }
+})
